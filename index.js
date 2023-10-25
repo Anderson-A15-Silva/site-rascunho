93e30eb8b6ba766e16a395d2efd1cf38bb1e7275
dt <denersonluz@gmail.com>
2023-10-25 20:24:43 -0300

    Adicionando alterações ao menu do site

---
+++ index.js
@@ -0,0 +1,11 @@
+var pNome = document.getElementById("pNome");
+var nome = Number(pIdade);
+
+var pIdade = document.getElementById("pIdade");
+var idade = Number(pIdade);
+
+var pSexo = document.getElementById("pSexo");
+var sexo = Number(pSexo);
+
+var pIQ = document.getElementById("pIQ");
+var iq = Number(pIQ);
