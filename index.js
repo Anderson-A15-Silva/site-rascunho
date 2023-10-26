fc47cf7feafa5c488af0168e141af93f7ddb3a16
dt <denersonluz@gmail.com>
2023-10-25 21:32:14 -0300

    Adicionando alteraações ao site

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var pNome = document.getElementById("pNome");
-var nome = Number(pIdade);
+var nome = pNome;
 
 var pIdade = document.getElementById("pIdade");
 var idade = Number(pIdade);
@@ -8,4 +8,31 @@ var pSexo = document.getElementById("pSexo");
 var sexo = Number(pSexo);
 
 var pIQ = document.getElementById("pIQ");
-var iq = Number(pIQ);
+var iq = pIQ;
+
+var pFor = document.getElementById("pFor");
+var forca = pFor;
+
+var pVig = document.getElementById("pVig");
+var vigor = pVig;
+
+var pAgi = document.getElementById("pAgi");
+var agilidade = pAgi;
+
+var pInt = document.getElementById("pInt");
+var intelecto  = pInt;
+
+var pPre = document.getElementById("pPre");
+var presenca = pPre;
+
+
+function exibirValores() {
+    console.log("Nome :", nome)
+    console.log("Nome :", idade)
+    console.log("Nome :", sexo)
+    console.log("Nome :", iq)
+    console.log("Nome :", forca)
+    console.log("Nome :", agilidade)
+    console.log("Nome :", intelecto)
+    console.log("Nome :", presenca)
+}
